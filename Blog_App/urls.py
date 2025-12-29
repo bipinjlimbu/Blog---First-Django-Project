fde140963feb285ed9bb1f2f5010a6bbbadb1d63
bipinjlimbu <bipinlimbu712@gmail.com>
2025-12-29 13:18:47 +0545

    Bug Fixed

--- Blog_App/urls.py
+++ Blog_App/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from views.main_view import index_page
-from views.auth_view import login_page, register_page
+from .views.main_view import index_page
+from .views.auth_view import login_page, register_page
 
 urlpatterns = [
     path('', index_page, name='index_page'),
