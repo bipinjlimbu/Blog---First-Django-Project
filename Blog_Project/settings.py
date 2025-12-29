a4e8f88e49ba0500b25fd1332de397110cdb904e
bipinjlimbu <bipinlimbu712@gmail.com>
2025-12-29 13:36:27 +0545

    Database Connected

--- Blog_Project/settings.py
+++ Blog_Project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Blog_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog_project_django',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
