60b2572281a6419611c7b95a4a900cd6815b18d6
bipinjlimbu <bipinlimbu712@gmail.com>
2025-12-30 08:53:57 +0545

    Register Page View Updated

--- Blog_App/views/auth_view.py
+++ Blog_App/views/auth_view.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
+def register_page(request):
+    errors = {}
+    if request.method == 'POST':
+        username = request.POST.get('username', '').strip()
+        first_name = request.POST.get('first_name', '').strip()
+        last_name = request.POST.get('last_name', '').strip()
+        email = request.POST.get('email', '').strip()
+        password = request.POST.get('password', '').strip()
+        confirm_password = request.POST.get('confirm_password', '').strip()
+
+        
+    return render(request, 'auth/register_page.html')
+
 def login_page(request):
     return render(request, 'auth/login_page.html')
-
-def register_page(request):
-    return render(request, 'auth/register_page.html')
