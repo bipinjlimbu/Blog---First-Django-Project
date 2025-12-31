e862ddc1ac9a1cc6eacaeea4939eb4feebf4de2b
bipinjlimbu <bipinlimbu712@gmail.com>
2025-12-31 13:06:39 +0545

    Blogs Model Created

--- Blog_App/models.py
+++ Blog_App/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Blogs(models.Model):
+    title = models.CharField(max_length=200)
+    category = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='blog_images/', null=True, blank=True)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
