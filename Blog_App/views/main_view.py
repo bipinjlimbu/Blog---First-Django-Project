6966405796b9b402c5933e289436bf6ff43230f5
bipinjlimbu <bipinlimbu712@gmail.com>
2025-12-31 12:51:44 +0545

    Login Required Conditions Added

--- Blog_App/views/main_view.py
+++ Blog_App/views/main_view.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def index_page(request):
     return render(request, 'main/index_page.html')
 
+@login_required
 def create_blog(request):
     return render(request, 'main/create_blog.html')
