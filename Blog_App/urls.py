80ac3195b996197b783c591c0d4f953d05305b9b
bipinjlimbu <bipinlimbu712@gmail.com>
2025-12-31 12:28:45 +0545

    Create Blog Added

--- Blog_App/urls.py
+++ Blog_App/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views.main_view import index_page
+from .views.main_view import index_page, create_blog
 from .views.auth_view import login_page, register_page, logout_view
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', login_page, name='login'),
     path('register/', register_page, name='register'),
     path('logout/', logout_view, name='logout'),
+    path('create-blog/', create_blog, name='create_blog'),
 ]
--- Blog_App/views/main_view.py
+++ Blog_App/views/main_view.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index_page(request):
     return render(request, 'main/index_page.html')
+
+def create_blog(request):
+    return render(request, 'main/create_blog.html')
